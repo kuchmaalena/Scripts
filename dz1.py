55eaf073d3ccef523da420488c757181341ceb86
kuchmaalena <kuchma.a.v@mail.ru>
2019-05-20 22:55:30 +0300

    Update dz1.py

--- dz1.py
+++ dz1.py
@@ -1,10 +1,7 @@
 def is_permutation(a:str, b:str):
     s1 = set(a)
     s2 = set(b)
-    if s1.union(s2) == s1:
-        return(True)
-    else:
-        return(False)
-        
+    return(s1.union(s2) == s1)
+ 
            
                     
